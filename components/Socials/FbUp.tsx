85c626099cb5773aed2f2cc06d5e1825d1cf255c
rini <reneortega2468@gmail.com>
2022-12-01 10:17:25 -0500

    oAuth2.0

--- components/Socials/FbUp.tsx
+++ components/Socials/FbUp.tsx
@@ -1,40 +1,37 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 
 const FbUp = () => {
   return (
     // make a small circle with a google logo
     <View style={styles.container}>
-        <TouchableOpacity>
-      <View style={styles.circle}>
-
-        <Text style={styles.text}>F</Text>
-        
-      </View>
-        </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.circle}>
+          <Text style={styles.text}>F</Text>
+        </View>
+      </TouchableOpacity>
     </View>
+  );
+};
 
-  )
-}
-
-export default FbUp
+export default FbUp;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   circle: {
     width: 50,
     height: 50,
     borderRadius: 50 / 2,
-    backgroundColor: '#DEB887',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#DEB887",
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
   },
-})
+});
