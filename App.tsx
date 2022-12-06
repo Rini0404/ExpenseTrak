c7de90b6f5bf81478032bc0c8129c88b37f407f9
Rene Ortega <reneortega2468@gmail.com>
2022-12-06 07:45:03 -0500

    Working on auth

--- App.tsx
+++ App.tsx
@@ -5,9 +5,20 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+
+import * as Linking from 'expo-linking';
+
+const prefix = Linking.createURL('/');
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const linking = {
+    prefixes: [prefix],
+  };
+
 
   if (!isLoadingComplete) {
     return null;
